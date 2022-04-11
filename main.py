40756890db6349d89774c6895876459c1923927f
TrainDoctor <11465594+TrainDoctor@users.noreply.github.com>
2022-04-11 12:54:48 -0700

    Started on methods

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import sys
-sys.path.insert(0, "")
+import os,sys
+sys.path.insert(0, os.getcwd()+"/vdf")
+print(sys.path[0])
 
 class Plugin:
     # The name of the plugin. This string will be displayed in the Plugin menu
     name = "Performance Presets"
-    # The name of the plugin author
+    # The name of the plugin author 
     author = "TrainDoctor"
 
     # If the plugin should be reloaded from a call to /plugins/reload or a file change
@@ -17,13 +18,22 @@ class Plugin:
     # Comment this out if you don't plan to use a tile view. This will make a button with your plugin name appear
     tile_view_html = ""
 
-    # A normal method. It can be called from JavaScript using call_plugin_function("method_1", argument1, argument2)
-    async def method_1(self, *args):
+    # Return true if a game is currently active, else return false
+    async def game_active(self, *args):
+        pass
+    
+    async def save_preset(self, *args):
+        pass
+    
+    async def load_preset(self, *args):
+        pass
+    
+    async def delete_preset(self, *args):
         pass
 
     # A normal method. It can be called from JavaScript using call_plugin_function("method_2", argument1, argument2)
-    async def method_2(self, *args):
-        pass
+    # async def method_2(self, *args):
+    #     pass
 
     # Asyncio-compatible long-running code, executed in a task when the plugin is loaded
     async def __main(self):
