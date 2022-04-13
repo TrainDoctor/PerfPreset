cb4e94bfdf92785d3bc3384a864af901a6b809a6
TrainDoctor <11465594+TrainDoctor@users.noreply.github.com>
2022-04-13 14:13:14 -0700

    that was silly

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import os,shutil,subprocess
 import tempfile
 
 class Plugin:
-    first_run = "True"
-    
     temp_config = "/dev/null"
 
     # Asyncio-compatible long-running code, executed in a task when the plugin is loaded
@@ -12,10 +10,7 @@ class Plugin:
         config_file = "/config/config.vdf"
         temp = tempfile.NamedTemporaryFile(delete=True)
         shutil.copy2(steam_directory+config_file, temp.name) # for diffing if need be
-        if os.path.exists(steam_directory+config_file+".bak"):
-            Plugin.first_run = "False"
-        # TODO: do not use shutil and use a bash subprocess to copy instead to preserve file info
-        if Plugin.first_run:
+        if not os.path.exists(steam_directory+config_file+".bak"):
             shutil.copy(steam_directory+config_file,steam_directory+config_file+".bak") # preserve a backup of our original file jic
+        # TODO: do not use shutil and use a bash subprocess to copy instead to preserve file info
         Plugin.temp_config = temp.name
-    pass
