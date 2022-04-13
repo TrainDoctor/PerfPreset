8e86090f087bb23e00f9df927e00bbf35f85f4c7
TrainDoctor <11465594+TrainDoctor@users.noreply.github.com>
2022-04-13 14:10:15 -0700

    I don't think I need root

--- main.py
+++ main.py
@@ -2,13 +2,6 @@ import os,shutil,subprocess
 import tempfile
 
 class Plugin:
-    # A normal method. It can be called from JavaScript using call_plugin_function("method_1", argument1, argument2)
-    async def method_1(self, *args):
-        pass
-
-    # A normal method. It can be called from JavaScript using call_plugin_function("method_2", argument1, argument2)
-    async def method_2(self, *args):
-        pass
     first_run = "True"
     
     temp_config = "/dev/null"
